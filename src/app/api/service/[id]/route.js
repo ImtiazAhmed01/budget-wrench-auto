7eeb6ca9ea648fe0176b1f5809d0a17ac75f6d45
ImtiazAhmed01 <imtiazahm21@gmail.com>
2025-06-13 22:38:06 +0600

    delete working

--- src/app/api/service/[id]/route.js
+++ src/app/api/service/[id]/route.js
@@ -1,6 +1,33 @@
+import { authOptions } from "@/lib/authOptions";
 import { collectionNamesObj, dbConnect } from "@/lib/dbConnect";
 import { ObjectId } from "mongodb";
+import { getServerSession } from "next-auth";
+import { revalidatePath } from "next/cache";
 import { NextResponse } from "next/server";
+
+export const DELETE = async (req, { params }) => {
+    const bookingCollection = await dbConnect(collectionNamesObj.bookingCollection)
+
+    const p = await params;
+    const query = { _id: new ObjectId(p.id) }
+    const session = await getServerSession(authOptions)
+    const currentBooking = await bookingCollection.deleteOne(query);
+
+    const isOwnerOk = session?.user?.email == currentBooking.email
+    if (isOwnerOk) {
+        const deleteResponse = await serviceCollection.deleteOne(query);
+        revalidatePath('/my-bookings')
+        return NextResponse.json(deleteResponse)
+    }
+    else {
+        return NextResponse.json({ success: false, message: "Forbidden Action" }, { status: 401 })
+    }
+
+
+    // const serviceCollection = await dbConnect(collectionNamesObj.servicesCollection);
+
+
+}
 
 export const GET = async (req, { params }) => {
     const p = await params;
--- src/app/my-bookings/components/DeleteBookingButton.jsx
+++ src/app/my-bookings/components/DeleteBookingButton.jsx
@@ -6,7 +6,8 @@ export default function DeleteBookingButton({ id }) {
     const router = useRouter();
     const handleDelete = async (id) => {
         const res = await fetch(
-            `https://nextjs-car-doctor-kappa.vercel.app/api/service/${id}`,
+            // `https://nextjs-car-doctor-kappa.vercel.app/api/service/${id}`,
+            `http://localhost:3000/api/service/${id}`,
             {
                 method: "DELETE",
             }
